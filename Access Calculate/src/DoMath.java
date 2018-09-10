69d797ffb616b3b3015f52f2bf84adfd4f859561
APCS3rd <APCS3rd@MHS-Math2-26>
2018-09-10 10:55:47 -0700

    adding calculate library

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+//This class is client code that uses the calculate library to perform various mathematical tasks. 
+//@Carolyn Yee
+//@version 1, 9/06/18
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
