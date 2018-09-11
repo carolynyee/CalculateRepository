6e5c3c5d088304f5553ce8934700e62bd21fe981
APCS3rd <APCS3rd@MHS-Math2-26>
2018-09-11 11:07:02 -0700

    part 1 of calculate library (in progress)

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -4,6 +4,13 @@
 public class DoMath {
 
 	public static void main(String[] args) {
+		Calculate.square(5);
+			System.out.println(Calculate.square(5));
+			System.out.println(Calculate.cube(5));
+			System.out.println(Calculate.average(1.2, 5.6));
+			System.out.println(Calculate.average(4.3, 6.7, 8.2));
+			System.out.println(Calculate.toDegrees(3.14159));
+			System.out.println(Calculate.toRadians(180));
 		// TODO Auto-generated method stub
 
 	}
